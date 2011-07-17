70df05754dfd8decd96419328158b0de91e2a228
Yi Wei <genius.ron@gmail.com>
2011-07-16 20:19:40 -0500

    new file:   WebContent/views/welcome.jsp

---
+++ src/com/yfox/webcanvas/WelcomeServlet.java
@@ -0,0 +1,17 @@
+package com.yfox.webcanvas;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class WelcomeServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+	throws ServletException, java.io.IOException {
+		RequestDispatcher welcomeDispatcher = request.getRequestDispatcher("/views/welcome.jsp");
+		welcomeDispatcher.forward(request, response);
+	}
+}
